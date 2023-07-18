5fd8cd388c4fa42b13517ba9925196d011557d66
Danilo Silva <danilo.silva@msn.com>
2023-07-18 00:47:35 -0300

    Configurando o JWT na API

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Program.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Program.cs
@@ -1,7 +1,11 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,40 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+builder.Services.AddAuthentication(options =>
+{
+    // DefaultAuthenticateScheme: Define o esquema padrão para autenticação.
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    // DefaultChallengeScheme: Define o esquema padrão para desafio de autenticação.
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>
+{
+    // RequireHttpsMetadata: Define se o HTTPS é obrigatório ou não.
+    bearerOptions.RequireHttpsMetadata = true;
+    // SaveToken: Define se o token deve ser gravado no contexto HTTP.
+    bearerOptions.SaveToken = true;
+    // TokenValidationParameters: Define os parâmetros de validação do token.
+    bearerOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        // ValidateIssuerSigningKey: Define se a chave de assinatura deve ser validada.
+        ValidateIssuerSigningKey = true,
+        // IssuerSigningKey: Define a chave de assinatura.
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        // ValidateIssuer: Define se o emissor deve ser validado.
+        ValidateIssuer = true,
+        // ValidateAudience: Define se o público deve ser validado.
+        ValidAudience = appSettings.ValidoEm,
+        // ValidateIssuer: Define se o emissor deve ser validado.
+        ValidIssuer = appSettings.Emissor,
+    };
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
