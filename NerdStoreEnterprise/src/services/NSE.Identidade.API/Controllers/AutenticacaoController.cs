6e89e1006474e606414a03ff43cdd7d59dc231fa
Danilo Silva <danilo.silva@msn.com>
2023-07-19 00:50:55 -0300

    Customizando as mensagens do Identity

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Controllers/AutenticacaoController.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Controllers/AutenticacaoController.cs
@@ -10,9 +10,8 @@ using NSE.Identidade.API.Models;
 
 namespace NSE.Identidade.API.Controllers;
 
-[ApiController]
 [Route("api/identidade")]
-public class AutenticacaoController : Controller
+public class AutenticacaoController : MainController
 {
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
@@ -31,7 +30,7 @@ public class AutenticacaoController : Controller
     [HttpPost("nova-conta")]
     public async Task<ActionResult> Registrar(RegistroViewModel registroViewModel)
     {
-        if (!ModelState.IsValid) return BadRequest();
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         var usuario = new IdentityUser
         {
@@ -45,27 +44,38 @@ public class AutenticacaoController : Controller
         if (usuarioCriado.Succeeded)
         {
             // isPersistent: Define se o cookie de autenticação será persistente ou não 
-            await _signInManager.SignInAsync(usuario, false);
-            return Ok(await GerarJwt(registroViewModel.Email));
+            return CustomResponse(await GerarJwt(registroViewModel.Email));
         }
 
-        return BadRequest();
+        foreach (var erro in usuarioCriado.Errors)
+        {
+            AdicionarErroProcessamento(erro.Description);
+        }
+
+        return CustomResponse();
     }
 
     [HttpPost("autenticar")]
     public async Task<ActionResult> Login(LoginViewModel loginViewModel)
     {
-        if (!ModelState.IsValid) return BadRequest();
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         // lockoutOnFailure: Define se o usuário será bloqueado após várias tentativas de login
         var usuarioAutenticado = await _signInManager.PasswordSignInAsync(loginViewModel.Email, loginViewModel.Senha, false, true);
 
         if (usuarioAutenticado.Succeeded)
         {
-            return Ok(await GerarJwt(loginViewModel.Email));
+            return CustomResponse(await GerarJwt(loginViewModel.Email));
         }
 
-        return BadRequest();
+        if (usuarioAutenticado.IsLockedOut)
+        {
+            AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas");
+            return CustomResponse();
+        }
+
+        AdicionarErroProcessamento("Usuário ou senha incorretos");
+        return CustomResponse();
     }
 
     private async Task<LoginResponseViewModel> GerarJwt(string email)
